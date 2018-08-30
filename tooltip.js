36f65697774fa5bf9e6ca0bcb59916c556a8b43d
Caro <Caroline9L@users.noreply.github.com>
2018-08-30 13:21:56 -0400

    Add files via upload

---
+++ tooltip.js
@@ -0,0 +1,9 @@
+function showTooltip() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function hideTooltip() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("hidden");
+}
